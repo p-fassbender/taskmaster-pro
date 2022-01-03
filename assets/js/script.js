cc83307b5b6dc203db01496f9c905e587f072a53
Preston Fassbender <fassbenderp0551@gmail.com>
2022-01-03 11:32:58 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,7 +157,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
     // get form values
     var taskText = $("#modalTaskDescription").val();
     var taskDate = $("#modalDueDate").val();
@@ -194,15 +194,23 @@ $(".card .list-group").sortable({
     helper: "clone",
     activate: function (event) {
         console.log("activate", this);
+        $(this).addClass("dropover");
+        $(".bottom-trash").addClass("bottom-trash-drag");
     },
     deactivate: function (event) {
         console.log("deactivate", this);
+        $(this).removeClass("dropover");
+        $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function (event) {
         console.log("over", event.target);
+        $(event.target).addClass("dropover-active");
+        
     },
     out: function (event) {
         console.log("out", event.target);
+        $(event.target).removeClass("dropover-active");
+        
     },
     update: function (event) {
         // array to store the task data in
@@ -244,12 +252,15 @@ $("#trash").droppable({
     drop: function (event, ui) {
         console.log("drop");
         ui.draggable.remove();
+        $(".bottom-trash").removeClass("bottom-trash-active");
     },
     over: function (event, ui) {
         console.log("over");
+        $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function (event, ui) {
         console.log("out");
+        $(".bottom-trash").removeClass("bottom-trash-active");
     }
 });
 
@@ -274,6 +285,12 @@ var auditTask = function(taskEl) {
         $(taskEl).addClass("list-group-item-warning");
     }
 };
+
+setInterval(function() {
+    $(".card .list-group-item").each(function(index, el) {
+        auditTask(el);
+    });
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
